96e1ca840c277ef918fb095819bcbab0680d9173
marquis brunbdage <marquisbrunbdage@marquiss-MacBook-Pro.local>
2023-10-16 19:40:19 -0400

    struggled with understanding functions'
    q
    git status
    !
    mesagesent!
    messagesent!
    'messagesent'!
    %

---
+++ script.js
@@ -0,0 +1,54 @@
+
+const fastCars  = [
+  {id:1,cars:"Aston Martin One-77", mph: 220,} ,
+  {id:2,cars:"Lamborghini Aventador LP 780-4 Ultimae",mph:221}, 
+  {id:3,cars:"Gordon Murray Automotive T.50 ",mph: 230}, 
+  {id:4,cars:"Pagani Huayra",mph: 230},
+  {id:5,cars:"Pagani Huayra BC Roadster", mph: 240 },
+  {id:6,cars:"Saleen S7 Twin Turbo", mph: 248}
+];
+
+const cars = prompt('Enter the speed of a car that goes from 220 to 250');
+const speed = fastCars.push(1);
+console.log(speed)
+
+
+
+  
+    // if( speed >= 220, speed  <= 240){
+    //   fastCars.push(0)
+    //   fastCars.push(1)
+    //   fastCars.push(2)
+    //   fastCars.push(3)
+    //   fastCars.push(4)
+    //   console.log(`These are the cars ${fastCars} with the mph from 220 to 238`);    
+  
+    //  }
+    //  else{
+    //   (speed >= 240)
+    //      fastCars.push(6)
+    //   console.log(`These are the cars ${fastCars} with the mph from 220 to 238`);
+    //  }
+   
+
+
+//  if(speed < 240){
+//   console.log("this are the cars ranging from  220 to 2230")
+//   console.log(speed)
+  
+
+//  }
+//  function getCar(speed){
+//   const output=[]
+  
+//   if(speed > input +240)
+//   output.push(speed[0])
+//   output.push(speed[1])
+//   output.push(speed[3])
+//   console.log(speed)
+//   return output;
+//  }
+
+
+
+
